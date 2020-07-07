982ab4f1c151c4b9033b54634aab9b291feaf4e5
Adrián Antón <adrtler@gmail.com>
2020-07-07 19:15:24 +0200

    docs agregado

---
+++ docs/precache-manifest.01081e5e7ffe1c28d1fbfe1b90f717b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64261a9d94b431e71ac008b54fbf435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ac87337a95d42173cae",
+    "url": "/static/css/main.0685ec17.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac87337a95d42173cae",
+    "url": "/static/js/main.34c26830.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
